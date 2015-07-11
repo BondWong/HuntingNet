2b3cb62fb949b1f02a0e3242ca68ce41da994ca8
JavaEEBoy <WongZeonbong@gmail.com>
2015-07-11 15:37:00 +0800

    how to page is ready and awaites for 5 images

--- WebContent/HuntingWeb-v2/js/huntingweb.js
+++ WebContent/HuntingWeb-v2/js/huntingweb.js
@@ -1,20 +1,29 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop : $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+	target : '.navbar-fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+	$('.navbar-toggle:visible').click();
 });
+
+// wechat public platform show
+/*
+ * $("#wechat").mouseover(function() { $("#wechat_qrcode").css("display",
+ * "block"); });
+ * 
+ * $("#wechat").mouseout(function() { $("#wechat_qrcode").css("display",
+ * "none"); })
+ */;
